394c6be6cd2664149bf618412c1b7cba6f158f0f
Dexsam07 <txgaminglover2@gmail.com>
2026-01-24 18:47:15 +0530

    Add files via upload

--- commands/clearsession.js
+++ commands/clearsession.js
@@ -8,8 +8,8 @@ const channelInfo = {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '120363406449026172@newsletter',
+            newsletterName: 'DexBot MD',
             serverMessageId: -1
         }
     }
